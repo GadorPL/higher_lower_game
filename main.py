0bf5872f1bdd0f3883de77ee616e8e40b89bc107
Michał Hewelt <mhewelt9@gmail.com>
2022-12-05 12:51:29 +0100

    place account_b in place of account_a after correct answer

--- main.py
+++ main.py
@@ -3,30 +3,30 @@ import random
 
 score = 0
 account_a = random.choice(data)
-account_b = random.choice(data)
-print(f"Compare A: {account_a['name']}, {account_a['description']}, from {account_a['country']}.")
-print(f"Compare B: {account_b['name']}, {account_b['description']}, from {account_b['country']}.")
+game_over = False
+
+while not game_over:
+    account_b = random.choice(data)
+    print(f"Compare A: {account_a['name']}, {account_a['description']}, from {account_a['country']}.")
+    print(f"Compare B: {account_b['name']}, {account_b['description']}, from {account_b['country']}.")
+
+    user_answer = input("Who has more followers? Type 'A' or 'B': ").upper()
+    account_a_follower_count = account_a['follower_count']
+    account_b_follower_count = account_b['follower_count']
+
+    if account_a_follower_count > account_b_follower_count:
+        correct_answer = 'A'
+    else:
+        correct_answer = 'B'
+
+    if correct_answer == user_answer:
+        score += 1
+        print(f"You're right! Current score: {score}")
+        account_a = account_b
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+        game_over = True
 
 
-user_answer = input("Who has more followers? Type 'A' or 'B': ").upper()
-account_a_follower_count = account_a['follower_count']
-account_b_follower_count = account_b['follower_count']
-
-if account_a_follower_count > account_b_follower_count:
-    correct_answer = 'A'
-else:
-    correct_answer = 'B'
-
-if correct_answer == user_answer:
-    score += 1
-    print(f"You're right! Current score: {score}")
-else:
-    print(f"Sorry, that's wrong. Final score: {score}")
-    game_over = True
-
-
-# TODO: 3 Place account B in place of account A and then take random new account as a choice B
 # TODO: Add logos
 # TODO: Make sure there can never be compared 2 accounts with the same number of followers
-
-
