54a22ae10acfc550fab0ad1f77143b21506a62dc
Michał Hewelt <mhewelt9@gmail.com>
2022-12-05 12:15:45 +0100

    display two random accounts

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from game_data import data
+import random
+
+account_a = random.choice(data)
+account_b = random.choice(data)
+print(f"Compare A: {account_a['name']}, {account_a['description']}, from {account_a['country']}.")
+print(f"Compare B: {account_b['name']}, {account_b['description']}, from {account_b['country']}.")
+
+# TODO: 2 Compare logic
+# TODO: 3 Place account B in place of account A and then take random new account as a choice B
+# TODO: Add logos
+
 
